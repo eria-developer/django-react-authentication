3a90171a0f4da0b227d2e89943990b2fc8ce02e6
eria-developer <eriddeveloper@gmail.com>
2024-06-28 19:39:36 +0300

    set up the user model

--- backend/authentication/models.py
+++ backend/authentication/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.utils.translation import gettext_lazy as _ 
 
-# Create your models here.
+
+class user(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=254, unique=True, verbose_name=_("Email Address"))
+    first_name = models.CharField(max_length=254, verbose_name=_("First Name"))
+    last_name = models.CharField(max_length=254, verbose_name=_("Last Name"))
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_verified = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELDS = 'email'
+
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    objects = UserManager()
+
+    def __str__(self):
+        return f'{self.email}'
+    
+    @property
+    def get_fullname(self):
+        return f'{self.first_name} {self.last_name}'
+    
+    def tokens(self):
+        pass
