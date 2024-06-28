37d043a24d12106953a98124c867007978e2dab9
eria-developer <eriddeveloper@gmail.com>
2024-06-28 21:03:25 +0300

    created serializer for user register

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework.generics import GenericAPIView
+from .serializers import UserRegistrationSerializer
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+
+class RegisterUserView(GenericAPIView):
+    serializer_class = UserRegistrationSerializer
+
+    def post(self, request):
+        user_data = request.data 
+        serializer=self.serializer_class(data=user_data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            user=serializer.data()
+            # send email function user["email"]
+            return Response({
+                "data": user,
+                "message": f"hi {user.first_name}, thanks for signing up"
+            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
