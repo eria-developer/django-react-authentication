69002767e07fd10a1798e27ff0f1f0a5d027d6a0
eria-developer <eriddeveloper@gmail.com>
2024-06-30 18:11:21 +0300

    set up the signup page template

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 const Signup = () => {
   return (
-    <div>Signup</div>
-  )
-}
+    <div>
+      <div className="form-container">
+        <div style={{ width: "100%" }} className="wrapper">
+          <h2>create account</h2>
+          <form action="" onSubmit={""}>
+            <div className="form-group">
+              <label htmlFor="">Email Address:</label>
+              <input type="text" className="email-form" name="email" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="">First Name:</label>
+              <input type="text" className="email-form" name="first_name" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="">Last Name:</label>
+              <input type="text" className="email-form" name="last_name" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="">Password:</label>
+              <input type="text" className="email-form" name="password" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="">Confirm Password:</label>
+              <input type="text" className="p" name="password2" />
+            </div>
+            <input type="submit" value="Submit" className="submitButton" />
+          </form>
+          <h3 className="text-option">Or</h3>
+          <div className="githubContainer">
+            <button>Sign up with Github</button>
+          </div>
+          <div className="googleContainer">
+            <div id="signInDiv" className="gsignIn"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Signup
+export default Signup;
