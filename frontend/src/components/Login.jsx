711c8e88a727c75475216921159c079a7ba325d3
eria-developer <eriddeveloper@gmail.com>
2024-06-30 23:05:56 +0300

    implemented login

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,33 +1,80 @@
 import React, { useEffect, useState } from "react";
 import { Link, useNavigate, useSearchParams } from "react-router-dom";
 import { toast } from "react-toastify";
+import axios from "axios";
 
 const Login = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  //   const [error, setError] = useState("");
+  const { email, password } = formData;
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!email || !password) {
+      toast.error("All fields are required");
+    } else {
+      //   const toastId = toast.loading("Logging in...");
+      const response = await axios.post(
+        "http://localhost:8000/api/v1/auth/login/",
+        formData
+      );
+      console.log(response.data);
+
+      const user = {
+        email: response.data.email,
+        name: response.data.full_name,
+      };
+
+      if (response.status === 200) {
+        localStorage.setItem("user", JSON.stringify(user));
+        localStorage.setItem(
+          "access",
+          JSON.stringify(response.data.access_token)
+        );
+        localStorage.setItem(
+          "refresh",
+          JSON.stringify(response.data.refresh_token)
+        );
+        navigate("/dashboard");
+        toast.success(`You are welcome ${response.data.full_name}`);
+      }
+    }
+  };
+
   return (
     <div>
       <div className="form-container">
         <div style={{ width: "100%" }} className="wrapper">
           <h2>Login into your account</h2>
-          <form action="" onSubmit={"handleSubmit"}>
+          <form action="" onSubmit={handleSubmit}>
             <div className="form-group">
               <label htmlFor="">Email Address:</label>
               <input
                 type="text"
                 className="email-form"
-                value={""}
+                value={email}
                 name="email"
-                onChange={""}
+                onChange={handleChange}
               />
             </div>
 
             <div className="form-group">
               <label htmlFor="">Password:</label>
               <input
-                type="text"
+                type="password"
                 className="email-form"
-                value={""}
+                value={password}
                 name="password"
-                onChange={""}
+                onChange={handleChange}
               />
             </div>
 
@@ -38,7 +85,7 @@ const Login = () => {
           </form>
           <h3 className="text-option">Or</h3>
           <div className="githubContainer">
-            <button onClick={""}>Sign in with Github</button>
+            <button>Sign in with Github</button>
           </div>
           <div className="googleContainer">
             <div id="signInDiv" className="gsignIn"></div>
--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
-}
+  const user = JSON.parse(localStorage.getItem("user"));
+  const jwt_access = localStorage.getItem("access");
+  const navigate = useNavigate();
 
-export default Profile
+  useEffect(() => {
+    if (jwt_access === null || !user) {
+      navigate("/login");
+    }
+  }, []);
+
+  const handleLogout = () => {};
+
+  return (
+    <div className="container">
+      <h2>hi {user && user.name}</h2>
+      <p style={{ textAlign: "center" }}>welcome to your profile</p>
+      <button onClick={handleLogout} className="logout-btn">
+        Logout
+      </button>
+    </div>
+  );
+};
+
+export default Profile;
