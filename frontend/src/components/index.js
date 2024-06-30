89b8fce4bf612d930c1bf5367ff90a654e16888f
eria-developer <eriddeveloper@gmail.com>
2024-06-30 18:01:57 +0300

    set up the basic routes and components

---
+++ frontend/src/components/index.js
@@ -0,0 +1,7 @@
+import Login from "./Login";
+import Signup from "./Signup";
+import ForgotPassword from "./ForgotPassword";
+import Profile from "./Profile";
+import VerifyEmail from "./VerifyEmail";
+
+export { Login, Signup, ForgotPassword, Profile, VerifyEmail };
